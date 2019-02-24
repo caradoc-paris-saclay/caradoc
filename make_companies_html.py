ad44cbb689114612e782d76455a0154db425bfd9
Arnaud Gallant <gallant.arnaud@gmail.com>
2019-02-24 22:38:53 +0100

    Added the To Be Confirmed category to Company page

--- make_companies_html.py
+++ make_companies_html.py
@@ -7,20 +7,29 @@ html_path = join(root, "_includes/companies_array.html")
 
 html = open(html_path, "w")
 
+buffer = {}
 
 for root, dirs, files in walk(companies_folder):
 
         for f in sorted(files, key=str.lower):
+
                 if "DS_Store" in f:
                         pass
                 else:
+                        newroot = basename(root)
+                        newroot = basename(root) if "TBC" in newroot else "Confirmed " + newroot
                         name = basename(f)
                         name = name[0].upper() + name[1:-4]
+                        tbc_html = "" # "  <div class=center>On hold</div>\n" if "TBC" in newroot else ""
                         if "_" in name:
                                 name = name.replace("_", " ")
-                        html.write(
-                        "<li class=\"mix " + basename(root) + "\">\n" +
-                        "  <img src=\"{{ site.baseurl }}{% link /assets/img/companies/" +
-                           join(basename(root),basename(f)) + " %}\" " +
-                           "alt=\"" + name + "\">\n"+
-                        "</li>\n")
+
+                        buffer[name]="<li class=\"mix " + newroot + "\">\n" +\
+                                     "  <img src=\"{{ site.baseurl }}{% link /assets/img/companies/" +\
+                                        join(basename(root),basename(f)) + " %}\" " +\
+                                        "alt=\"" + name + "\">\n" +\
+                                      tbc_html +\
+                                      "</li>\n"
+
+for k in sorted(buffer.keys(), key=str.lower):
+    html.write(buffer[k])
