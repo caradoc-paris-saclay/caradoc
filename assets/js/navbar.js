a9357b443cf0168506bd2437e1caca1365117bf0
gh4ag <gallant.arnaud@gmail.com>
2020-01-31 02:37:20 +0100

    Finished implementing the navbar for dekstop version. Smartphone version left

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,24 @@
-<script>
-function toggleIndexSubNav() {
-  document.getElementById("panel").style.display = "block";
+var maxDeviceXidth = '(max-device-width:578px)'
+function deactivateSubNav(){
+		document.getElementById("event-sub-nav").style.display = "none";
+		document.getElementById("about-sub-nav").style.display = "none";
 }
-</script>
+function activateEventSubNav() {
+	deactivateSubNav();
+	//window.alert(event.target.id);
+ 	if (window.matchMedia(maxDeviceXidth).matches){
+ 		document.getElementById("event-sub-nav").style.display = "inherit";
+	}
+}
+function activateAboutSubNav() {
+	deactivateSubNav();
+	//window.alert(event.target.id);
+	console.log("Before condition");
+  if (window.matchMedia(maxDeviceXidth).matches){
+  	console.log("passed condition");
+  	document.getElementById("about-sub-nav").style.display = "inherit";
+  }
+}
+/*document.getElementById("EventMenu").addEventListener("click", activateEventSubNav);
+document.getElementById("AboutMenu").addEventListener("click", activateAboutSubNav);
+document.getElementById("AnyMenu").addEventListener("click", deactivateSubNav);*/
