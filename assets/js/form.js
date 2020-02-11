9ac5f0bd535a37fdf8c804be25f1fa10cc894725
gh4ag <gallant.arnaud@gmail.com>
2020-02-11 16:17:28 +0100

    working firebase registration form

---
+++ assets/js/form.js
@@ -0,0 +1,45 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCEMl2rBQqmY5YzqKGfYLy0VgLug7HQZ7o",
+  authDomain: "caradoc-b9cfd.firebaseapp.com",
+  databaseURL: "https://caradoc-b9cfd.firebaseio.com",
+  projectId: "caradoc-b9cfd",
+  storageBucket: "caradoc-b9cfd.appspot.com",
+  messagingSenderId: "1013854021090",
+  appId: "1:1013854021090:web:5268ed8763c40c9f1b45bb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Reference account collection
+
+var participantRef = firebase.database().ref('participants');
+
+//Listen to click on submit
+document.getElementById('registrationform').addEventListener('submit', submitForm);
+//Submit Form
+function submitForm(e){
+  e.preventDefault();
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+
+  console.log(name);
+  //save participant
+  saveParticipant(name, email);
+
+  document.getElementById('registrationform').reset();
+
+}
+
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+function saveParticipant(name, email){
+  var newParticipantRef = participantRef.push();
+  newParticipantRef.set({
+    name: name,
+    email: email
+  });
+}
