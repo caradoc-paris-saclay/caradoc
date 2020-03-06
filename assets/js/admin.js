43e3db1bab8b1f0df8d980387ac84ff06ab19dc7
gh4ag <gallant.arnaud@gmail.com>
2020-03-06 18:27:26 +0100

    improved admin login and layout + remove lab and admin page from sitemap

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,7 +4,7 @@ let provider = new firebase.auth.GoogleAuthProvider();
 var admin = firebase.auth();//('firebase-admin');
 var participant=null;
 var idRef=null;
-document.getElementById('admin_form').addEventListener('submit', login);
+//document.getElementById('login_form').addEventListener('submit', window.login);
 
 // window.updateForm 
 console.log(firebase.app().name);
@@ -21,9 +21,96 @@ firebase.auth().onAuthStateChanged(function(user) {
 	console.log('Error fetching user data:', error);
 });*/	
 
-function login(){
-	var email = document.getElementById("email").value;
-	var password = document.getElementById("password").value;
+var f = document.querySelector('#login_form');
+
+function toggleSignIn() {
+ 	if (firebase.auth().currentUser) {
+	    // [START signout]
+	    firebase.auth().signOut();
+	    // [END signout]
+  	} 
+  	else {
+	    var email = document.getElementById('username').value;
+	    var password = document.getElementById('password').value;
+	    if (email.length < 4) {
+	      alert('Please enter an email address.');
+	      return;
+	    }
+	    if (password.length < 4) {
+	      alert('Please enter a password.');
+	      return;
+		    }
+	    // Sign in with email and pass.
+	    // [START authwithemail]
+	    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+	      // Handle Errors here.
+	      var errorCode = error.code;
+	      var errorMessage = error.message;
+	      // [START_EXCLUDE]
+	      if (errorCode === 'auth/wrong-password') {
+	        alert('Wrong password.');
+	      } 
+	      else {
+	        alert(errorMessage);
+	      }
+	      console.log(error);
+	      document.getElementById('quickstart-sign-in').disabled = false;
+      	// [END_EXCLUDE]
+    	});
+	    // [END authwithemail]
+	}
+  	document.getElementById('quickstart-sign-in').disabled = true;
+}
+
+function initApp() {
+      // Listening for auth state changes.
+      // [START authstatelistener]
+	firebase.auth().onAuthStateChanged(function(user) {
+
+		if (user) {
+		  // User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var isAnonymous = user.isAnonymous;
+			var uid = user.uid;
+			var providerData = user.providerData;
+			// [START_EXCLUDE]
+			document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+			document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+			document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+			document.getElementById('username_div').style.display = "none";
+			document.getElementById('password_div').style.display = "none";
+		  // [END_EXCLUDE]
+		} 
+		else {
+			// User is signed out.
+			// [START_EXCLUDE]
+			document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+			document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+			document.getElementById('quickstart-account-details').textContent = 'null';
+			document.getElementById('username_div').style.display = "block";
+			document.getElementById('password_div').style.display = "block";
+			// [END_EXCLUDE]
+		}
+		// [START_EXCLUDE silent]
+		document.getElementById('quickstart-sign-in').disabled = false;
+		// [END_EXCLUDE]
+	});
+	// [END authstatelistener]
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+
+window.onload = function() {
+  initApp();
+};
+
+window.login = function login(f){
+	//var email = document.getElementById("username").value;
+	//var password = document.getElementById("password").value;
+	var email = f.username.value;
+	var password = f.password.value;
 	firebase.auth().signInWithEmailAndPassword(email, password)
 	.then(function(stuff){
 		console.log("Success? ", stuff);
@@ -37,4 +124,5 @@ function login(){
   	console.alert(error.message);
   	// ...
 	});
+	return false;
 }
