dc214be75e495c14e11f9c5d032a66c1c2839ed3
gh4ag <gallant.arnaud@gmail.com>
2020-08-18 13:25:31 +0200

    added img elements for index.html. Extented index.html page. Corrected responsivity. Tidied CSS code.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,25 @@
-/* scrolls from #arrow-registration to #index-registration-top and leave some space for navbar and
-content of #index-registration-top
+
+/* #############################################################################
+Arrows scrolling
+############################################################################# */
+/* scrolls from #arrow-caradoc to #index-caradoc-top and leave some space for navbar
  */
+$(document).ready(function (){
+    $("#arrow-caradoc").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#index-caradoc-top").offset().top - $(".navbar").outerHeight()
+        }, 500);
+    });
+});
+/* scr*/
+$(document).ready(function (){
+    $("#arrow-paris-saclay").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#index-paris-saclay-top ").offset().top - $(".navbar").outerHeight()
+        }, 500);
+    });
+});
+
 $(document).ready(function (){
     $("#arrow-registration").click(function (){
         $('html, body').animate({
