dde7e86ad67d200156f61bed91a1316834c47a5a
gh4ag <gallant.arnaud@gmail.com>
2020-09-01 11:48:56 +0200

    solved the user loggin password prompt issue on all web browser

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -38,6 +38,7 @@ console.log("firebase.app().name:", firebase.app.name);
 /* #############################################################################
 window.onload
 => called when page is loaded
+=> define the default behavior of sign in page
 ############################################################################# */
 window.onload = function() {
 	console.log("window.location.pathname: ", window.location.pathname );
@@ -52,6 +53,31 @@ window.onload = function() {
 		CollectionData.timestamp = Date(); // initialize timestamp of CollectionData class
 		updateNumbers();
 	}
+	else if (pageName == "admin"){
+		// we override the default behavior of the form submit action
+		var form = document.querySelector('#login_form');
+		form.addEventListener('submit', async e => { 
+			console.log("Signing In");
+			e.preventDefault(); 
+			// here we use form.name (i.e. name="username" html attribute)
+		    var email = form.username.value; 
+		    var password = form.password.value; 
+		    console.log(email);
+		    if (email.length < 4) {
+		      alert('Please enter an email address.');
+		      return;
+		    }
+		    if (password.length < 4) {
+		      alert('Please enter a password.');
+		      return;
+			}
+		    // Sign in Firebase with email and password
+		    // Password prompt are activated by respecting 
+		    // the chain of events managed by createSession
+		    await createSession(email, password, e);
+			return false;
+		});
+	}
 };
 /* #############################################################################
 initApp is called only once at window loading time.
@@ -65,47 +91,33 @@ function initApp() {
       // [START authstatelistener]
 	firebase.auth().onAuthStateChanged(function(user) {
 		console.log("Init App onAuthStateChanged");
+		// see Firebase doc to explore user attributes
 		if (user) {
-			console.log("User signed in")
-		  	// Get user data from firebase
-			var displayName = user.displayName;
-			var email = user.email;
-			var emailVerified = user.emailVerified;
-			var photoURL = user.photoURL;
-			var isAnonymous = user.isAnonymous;
-			var uid = user.uid;
-			var providerData = user.providerData;
+			// User is signed in.
 			document.getElementById('logging-status').textContent='Signed in';
 			document.getElementById("log-out-btn").disabled = false;
 			document.getElementById("log-out-btn").style.visibility = "visible"; 
-			// [START_EXCLUDE]
-			if (pageName == "admin"){
-				window.location.pathname = "/dashboard";
-			}
 			if (pageName == "dashboard"){
 				document.getElementById('dashboard').style.display = "block";
 			}
-		  // [END_EXCLUDE]
 		} 
 		else {
 			// User is signed out.
-			// [START_EXCLUDE]
 			if (pageName == "admin"){
 				document.getElementById('admin_title').textContent = "Admin Log In Page";
+				document.getElementById("log-out-btn").disabled = true;
+				document.getElementById("log-out-btn").style.visibility = "hidden"; 
+				document.getElementById("login_form").reset(); 
 			}
 			else if (pageName == "dashboard"){
 				document.getElementById('dashboard').style.display = "none";
 			}
-			// [END_EXCLUDE]
 		}
-		// [START_EXCLUDE silent]
 		if (pageName == "admin"){
 			document.getElementById('sign-in').disabled = false;
 		}
-		// [END_EXCLUDE]
 	});
-	// [END authstatelistener]
-	console.log("Reached end of initApp");
+	console.log("End of initApp");
 }
 
 /* #############################################################################
@@ -118,64 +130,52 @@ window.logout = function logout(){
 		window.location.pathname = "/admin";
 	}
 }
-/* #############################################################################
-signIn
-############################################################################# */
-window.signIn = function signIn() {
-	console.log("toggleSignIn triggered");
- 	if (firebase.auth().currentUser) {
-	    // [START signout]
-	    firebase.auth().signOut();
-	    // [END signout]
-  	} 
-  	else {
-	    var email = document.getElementById('username').value;
-	    var password = document.getElementById('password').value;
-	    if (email.length < 4) {
-	      alert('Please enter an email address.');
-	      return;
-	    }
-	    if (password.length < 4) {
-	      alert('Please enter a password.');
-	      return;
-		}
-	    // Sign in with email and password
-	    // [START authwithemail]
-	    createSession(email, password);
-	    document.getElementById('username').required = false;
-	    document.getElementById('password').required = false;
-	    document.getElementById("login_form").reset(); // clear form
-	    // [END authwithemail]
-	}
-}
 
 /* #############################################################################
 createSession
 ############################################################################# */
-function createSession(email, password){
+function createSession(email, password, e){
 	console.log("createSession")
 	firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
 	.then(function() {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
-    return firebase.auth().signInWithEmailAndPassword(email, password).catch(async function(error) {
-	      // Handle Errors here.
-	      var errorCode = error.code;
-	      var errorMessage = error.message;
-	      // [START_EXCLUDE]
-	      if (errorCode === 'auth/wrong-password') {
-	        alert('Wrong password.');
-	      } 
-	      else {
-	        alert(errorMessage);
-	      }
-	      console.log(error);
-	      document.getElementById('sign-in').disabled = false;
-      	// [END_EXCLUDE]
-    	});;
+		console.log("Firebase signIn.")
+	    // Existing and future Auth states are now persisted in the current
+	    // session only. Closing the window would clear any existing state even
+	    // if a user forgets to sign out.
+	    // 
+	    // New sign-in will be persisted with session persistence.
+	    // return below is optional could be deleted (IMO)
+	    // signInWithEmailAndPassword query Firebase server to know if
+	    // user(email, password) is authorized
+	    // authorizations on set on via the Firebase Console
+    	return firebase.auth().signInWithEmailAndPassword(email, password)
+    	.then(profile => {
+    		// Here we propose to the web browser to save the resutls
+    		if (window.PasswordCredential) {
+		   		var c = navigator.credentials.create({password: e.target});
+		   		return navigator.credentials.store(c);
+			} 
+			else {
+			 	return Promise.resolve(profile);
+			}
+		})
+		.then(profile => {
+			// we redirect to dashboard after the browser had time to save the credentials
+			// this will only work if user succeeded to log ing
+			window.location.pathname = "/dashboard";
+		})
+    	.catch(async function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			if (errorCode === 'auth/wrong-password') {
+				alert('Wrong password.');
+			} 
+			else {
+				alert(errorMessage);
+			}
+			console.log(error);
+    	});
   })
   .catch(function(error) {
     // Handle Errors here.
@@ -261,9 +261,6 @@ function loadCollection(collection){
 		db.collection(collection).get()
 		.then(snapshot =>{
 			console.log("snapshot", snapshot);
-			console.log("snapshot.path", snapshot.path);
-			console.log("not empty?", !snapshot.empty);
-			console.log("empty?", snapshot.empty);
 			try{
 				document
 			}
@@ -292,20 +289,16 @@ function loadCollection(collection){
 loadCounters accesses the database and downloads the "counters" collection from Firebase
 it is used to update the numbers of documents per collection displayed on the dashboard
 ############################################################################# */
-
 async function loadCounters(){
 
 	console.log("Fetching counters from Firestore")
 	return new Promise((resolve, reject) => {
-		//var docRef = db.collection("counters").doc("counters");
 		db.collection("counters").doc("counters").get()
 		.then(snapshot =>{
 			console.log("snapshot", snapshot);
 			console.log("snapshot.path", snapshot.path);
-			console.log("not empty?", !snapshot.empty);
-			console.log("empty?", snapshot.empty);
 			if (!snapshot.empty){
-				console.log("Not empty");
+				console.log("Counter snapshot is not empty");
 				resolve(snapshot);
 				console.log("Counters downloaded.");
 			}
@@ -326,7 +319,6 @@ async function loadCounters(){
 updates the numbers of documents per collection displayed on the dashboard
 use loadCounters for retreiving data from Firestore.
 ############################################################################# */
-
 async function updateNumbers(){
 	console.log("Updating page numbers.")
 	// Create a reference to the cities collection
